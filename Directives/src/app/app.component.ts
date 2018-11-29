b6924aed369aeab36e8f885de07eb9ffc46c1c5a
jesusdelaluz <jfdelaluz@gmail.com>
2018-11-29 11:33:10 -0700

    ngIf and ngFor samples

--- Directives/src/app/app.component.ts
+++ Directives/src/app/app.component.ts
@@ -7,5 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1, 3, 5];
+  evenNumbers = [2, 4];
   onlyOdd = false;
+  onlyEven = false;
+
+  showAllNumbers() {
+    this.onlyOdd = false;
+    this.onlyEven = false;
+  }
+
+  showOnlyOdd() {
+    this.onlyOdd = true;
+    this.onlyEven = false;
+  }
+
+  showOnlyEven() {
+    this.onlyOdd = false;
+    this.onlyEven = true;
+  }
 }
