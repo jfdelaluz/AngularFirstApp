82a306d606056a0a85486df2c64eb660d7a88c92
jesusdelaluz <jfdelaluz@gmail.com>
2018-11-29 23:20:44 -0700

    Added HostBinding sample

--- Directives/src/app/better-highlight/better-highlight.directive.ts
+++ Directives/src/app/better-highlight/better-highlight.directive.ts
@@ -1,27 +1,35 @@
-import { Directive, OnInit, ElementRef, Renderer2, HostListener } from '@angular/core';
+import {
+  Directive,
+  OnInit,
+  ElementRef,
+  Renderer2,
+  HostListener,
+  HostBinding
+} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.textAlign') textAlign: string = 'left';
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
     this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
-    this.renderer.setStyle(this.elRef.nativeElement, 'text-align', 'center');
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
     this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
-    this.renderer.setStyle(this.elRef.nativeElement, 'text-align', 'center');
+    this.textAlign = 'center';
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
     this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
-    this.renderer.setStyle(this.elRef.nativeElement, 'text-align', 'center');
+    this.textAlign = 'left';
   }
 }
