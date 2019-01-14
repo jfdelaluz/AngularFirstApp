56a8173b1136a46ebdde5a1208cecdd25d265b55
jesusdelaluz <jfdelaluz@gmail.com>
2019-01-13 23:03:45 -0700

    Removed rxjs-compat library, adjusting code to work with rxjs library alone

--- Observables/src/app/home/home.component.ts
+++ Observables/src/app/home/home.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable } from 'rxjs/Rx';
-//import { Observable } from 'rxjs/Observable'; // Does not work for interval function (use previous import)
-//import 'rxjs/Rx'; // Does not work for interval function
-import { Observer } from 'rxjs/Observer';
-import { Subscription } from 'rxjs/Subscription';
+import { Observable, Observer, Subscription, interval } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -17,10 +14,12 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-    const myNumbers = Observable.interval(1000).map(
-      (data: number) => {
-        return data * 2;
-      }
+    const myNumbers = interval(1000).pipe(
+      map(
+        (data: number) => {
+          return data * 2;
+        }
+      )
     );
     this.numbersObsSubscription = myNumbers.subscribe(
       (number: number) => {
