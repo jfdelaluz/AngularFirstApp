674767192024d3beffa5e49dcf775ed8e1ca5a73
jesusdelaluz <jfdelaluz@gmail.com>
2019-01-15 18:02:03 -0700

    Assignment for Forms template driven

--- FormsTDAssignment/src/app/app.component.ts
+++ FormsTDAssignment/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') dataForm: NgForm;
+  subscriptionOptions = ['Basic', 'Advanced', 'Pro'];
+  subscriptionDefault = 'Advanced';
+  data = {
+    email: '',
+    subscription: '',
+    password: ''
+  };
+  submitted = false;
 
+  onSubmit() {
+    this.submitted = true;
+
+    this.data.email = this.dataForm.form.value.email;
+    this.data.subscription = this.dataForm.form.value.subscription;
+    this.data.password = this.dataForm.form.value.password;
+
+    this.dataForm.reset();
+  }
 }
